eae42bf1f3c0a5ffaee4337d93c3693c990f1148
toyman640 <falakoadebayor@gmail.com>
2023-11-06 16:18:22 +0100

    set up user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# app/models/user.rb
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  has_secure_password 
+
+end
